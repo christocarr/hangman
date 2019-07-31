4f2bbd6546d647cb679bd48ede67c2a39c674460
Chris Carr <christocarr@outlook.com>
2019-07-31 18:49:46 +0100

    Add element to display status of game

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const game1 = new Hangman('Goat', 5);
 
+//set up game status element
+const gameStatusEl = document.getElementById('gameStatusEl')
+
 //set up the word to guess to display on screen
 const wordToGuess = document.getElementById('wordToGuess')
 let puzzle = game1.getPuzzle()
 wordToGuess.textContent = puzzle
+gameStatusEl.textContent = game1.getMessage()
 
 //set the remaining guesses to display
 const guessesRemainingDisplay = document.getElementById('guessesRemainingDisplay')
@@ -15,6 +19,6 @@ window.addEventListener('keypress', (e) => {
   game1.getGuess(guess)
   puzzle = game1.getPuzzle()
   wordToGuess.textContent = puzzle
-
+  gameStatusEl.textContent = game1.getMessage()
   guessesRemainingDisplay.textContent = game1.guessesRemaining
 })
