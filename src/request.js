05c22b4a8dc10b0c641fbf25bed1f475abccd7d3
christocarr <christocarr@outlook.com>
2019-08-12 20:04:04 +0100

    Add request to Wordnik api to get random word

--- src/request.js
+++ src/request.js
@@ -1,10 +1,10 @@
 'use strict'
 
 const getGame = async (wordCount) => {
-  const response = await fetch(`http://puzzle.mead.io/puzzle?wordCount=${wordCount}`)
+  const response = await fetch(`https://api.wordnik.com/v4/words.json/randomWord?hasDictionaryDef=true&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&api_key=`)
   if (response.status === 200) {
     const data = await response.json()
-    return data.puzzle
+    return data.word
   } else {
     throw new Error('Unable to get new puzzle')
   }
