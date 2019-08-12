bc329d5a630c4d44350b1db2205b4127cddfc5e4
Chris Carr <christocarr@outlook.com>
2019-08-12 19:23:32 +0100

    Add request to wordnik api

--- src/request.js
+++ src/request.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const getGame = async (wordCount) => {
-  const response = await fetch(`http://puzzle.mead.io/puzzle?wordCount=${wordCount}`)
+  const response = await fetch(`https://api.wordnik.com/v4/words.json/randomWord?hasDictionaryDef=true&maxCorpusCount=1&minDictionaryCount=1&maxDictionaryCount=-1&maxLength=-1&api_key=9pt0pmi5w3rjmjm9muzunv9ynrayz9z54h5pimcsjjj6tz70a`)
   if (response.status === 200) {
     const data = await response.json()
     return data.puzzle
@@ -11,3 +11,5 @@ const getGame = async (wordCount) => {
 }
 
 export default getGame
+
+//https://api.wordnik.com/v4/words.json/randomWord?hasDictionaryDef=true&maxCorpusCount=1&minDictionaryCount=1&maxDictionaryCount=-1&maxLength=-1&api_key=9pt0pmi5w3rjmjm9muzunv9ynrayz9z54h5pimcsjjj6tz70a
